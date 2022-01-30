ce19b9aa32656ce593a25b0cbd53f96460baa96f
DESKTOP-UG4I2UL\Jacob <jacob@netkinetix.com>
2022-01-29 21:29:48 -0600

    Login done

--- TGBot/Controllers/HomeController.cs
+++ TGBot/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -13,6 +14,7 @@ using JSON = Newtonsoft.Json.JsonConvert;
 
 namespace TGBot.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         
